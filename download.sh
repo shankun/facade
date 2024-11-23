a621587d56a7744c1763555ef0d8c9b3fe316636
shankun <shankun@outlook.com>
2024-11-23 23:29:44 +0800

    fix: syntax error: unexpected end of file

--- download.sh
+++ download.sh
@@ -24,27 +24,23 @@ if [! -d "./cache" ]; then
     mkdir cache
 if
 
-main() {
-    echo "Starting deploy..."
+echo "Starting deploy..."
 
-    git config --global url."https://".insteadOf git://
-    ## $GITHUB_SERVER_URL is set as a default environment variable in all workflows, default is https://github.com
-    git config --global url."$GITHUB_SERVER_URL/".insteadOf "git@${GITHUB_HOSTNAME}":
+git config --global url."https://".insteadOf git://
+## $GITHUB_SERVER_URL is set as a default environment variable in all workflows, default is https://github.com
+git config --global url."$GITHUB_SERVER_URL/".insteadOf "git@${GITHUB_HOSTNAME}":
 
-    # needed or else we get 'doubious ...' error
-    echo "Disable safe directory check"
-    git config --global --add safe.directory '*'
+# needed or else we get 'doubious ...' error
+echo "Disable safe directory check"
+git config --global --add safe.directory '*'
 
-    wget -O cache/ithome-rss.xml "https://www.ithome.com/rss"
-    wget -O cache/the-paper.json "https://cache.thepaper.cn/contentapi/wwwIndex/rightSidebar"
+wget -O cache/ithome-rss.xml "https://www.ithome.com/rss"
+wget -O cache/the-paper.json "https://cache.thepaper.cn/contentapi/wwwIndex/rightSidebar"
 
-    echo "Pushing artifacts to ${TARGET_REPOSITORY}:main"
-    git config user.name "GitHub Actions"
-    git config user.email "github-actions-bot@users.noreply.${GITHUB_HOSTNAME}"
-    git add .
-    git commit -m "update news files."
-    git push --force origin
-    echo "update complete"
-}
-
-main "$@"
+echo "Pushing artifacts to ${TARGET_REPOSITORY}:main"
+git config user.name "GitHub Actions"
+git config user.email "github-actions-bot@users.noreply.${GITHUB_HOSTNAME}"
+git add .
+git commit -m "update news files."
+git push --force origin
+echo "update complete"
